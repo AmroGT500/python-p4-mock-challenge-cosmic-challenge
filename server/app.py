6229861c06093e1716aba29bfb07a8322f0cdadd
Ammar Hussain <ammar92.h@gmail.com>
2023-07-31 12:00:37 -0400

    Co-authored-by: pmacellaro <pmacellaro@users.noreply.github.com>

--- server/app.py
+++ server/app.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python3
-
 from models import db, Scientist, Mission, Planet
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 from flask_migrate import Migrate
 from flask import Flask, make_response, jsonify, request
 import os
@@ -10,21 +8,129 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get(
     "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
+api = Api(app)
 
+# Home route
 @app.route('/')
 def home():
-    return ''
+    return 'Home'
 
+# Scientist list resource
+class ScientistListResource(Resource):
+    def get(self):
+        scientists = Scientist.query.all()
+        return [scientist.to_dict() for scientist in scientists]
+
+    def post(self):
+        # Parse the request data
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True, help='Name cannot be empty.')
+        parser.add_argument('field_of_study', type=str, required=True, help='Field of study cannot be empty.')
+        args = parser.parse_args()
+
+        # Create the new scientist
+        scientist = Scientist(name=args['name'], field_of_study=args['field_of_study'])
+        db.session.add(scientist)
+        db.session.commit()
+
+        # Return the new scientist data
+        return scientist.to_dict(), 201
+
+# Scientist resource
+class ScientistResource(Resource):
+    def get(self, id):
+        scientist = Scientist.query.get(id)
+        if not scientist:
+            return {'error': 'Scientist not found'}, 404
+        return scientist.to_dict()
+
+    def patch(self, id):
+        # Find the scientist by ID
+        scientist = Scientist.query.get(id)
+        if not scientist:
+            return {'error': 'Scientist not found'}, 404
+
+        # Parse the request data
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str)
+        parser.add_argument('field_of_study', type=str)
+        args = parser.parse_args()
+
+        # Update the scientist data
+        if args['name']:
+            scientist.name = args['name']
+        if args['field_of_study']:
+            scientist.field_of_study = args['field_of_study']
+        db.session.commit()
+
+        # Return the updated scientist data
+        return scientist.to_dict(), 202
+    
+    def delete(self, id):
+        # Find the scientist by ID
+        scientist = Scientist.query.get(id)
+        if not scientist:
+            return {'error': 'Scientist not found'}, 404
+        
+        # Delete associated missions
+        missions = Mission.query.filter_by(scientist_id=id).all()
+        for mission in missions:
+            db.session.delete(mission)
+
+        # Delete the scientist
+        db.session.delete(scientist)
+        db.session.commit()
+
+        # Return empty response
+        return '', 204
+
+# Add resources to the API
+api.add_resource(ScientistListResource, '/scientists')
+api.add_resource(ScientistResource, '/scientists/<int:id>')
+
+# Planet list resource
+class PlanetListResource(Resource):
+    def get(self):
+        planets = Planet.query.all()
+        return [planet.to_dict() for planet in planets]
+
+# Add resource to the API
+api.add_resource(PlanetListResource, '/planets')
+
+# Missions resource
+class MissionsResource(Resource):
+    def post(self):
+        # Parse the request data
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True, help='Name cannot be empty.')
+        parser.add_argument('scientist_id', type=int, required=True, help='Scientist ID cannot be empty.')
+        parser.add_argument('planet_id', type=int, required=True, help='Planet ID cannot be empty.')
+        args = parser.parse_args()
+
+        # Check if scientist and planet exist
+        scientist = Scientist.query.get(args['scientist_id'])
+        planet = Planet.query.get(args['planet_id'])
+        if not scientist or not planet:
+            return {'error': 'Invalid scientist or planet ID'}, 400
+
+        # Create the new mission
+        mission = Mission(name=args['name'], scientist_id=args['scientist_id'], planet_id=args['planet_id'])
+        db.session.add(mission)
+        db.session.commit()
+
+        # Return the new mission data
+        return mission.to_dict(), 201
+
+# Add resource to the API
+api.add_resource(MissionsResource, '/missions')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
+
 convention = {
     "ix": "ix_%(column_0_label)s",
     "uq": "uq_%(table_name)s_%(column_0_name)s",
@@ -28,8 +29,11 @@ class Planet(db.Model, SerializerMixin):
     # Add relationship: A Planet has many Scientists through Missions
     scientists = db.relationship('Scientist', secondary='missions', back_populates='planets')
 
+    # Add relationship: A Planet has many Missions
+    missions = db.relationship('Mission', back_populates='planet')
+
     # Add serialization rules
-    serialize_rules = ('-missions.planet',)
+    serialize_rules = ('-missions.planet', '-scientists.missions',)
 
 
 class Scientist(db.Model, SerializerMixin):
@@ -41,18 +45,27 @@ class Scientist(db.Model, SerializerMixin):
 
     # Add relationship: A Scientist has many Planets through Missions
     planets = db.relationship('Planet', secondary='missions', back_populates='scientists')
-
+    
+    # Add relationship: A Scientist has many Missions
+    missions = db.relationship('Mission', back_populates='scientist')
 
     # Add serialization rules
-    serialize_rules = ('-missions.scientist',)
+    serialize_rules = ('-missions.scientist', '-planets.missions', '-missions.planet', '-planets.scientists',)
 
-    # Add validation
+    # Add validation for name and field_of_study
     @validates('name')
     def validate_name(self, key, name):
-    # custom validation logic
-        if len(name.strip()) == 0:
+        # custom validation logic
+        if not name.strip():
             raise ValueError("Name cannot be empty.")
         return name
+    
+    @validates('field_of_study')
+    def validate_field_of_study(self, key, field_of_study):
+        # custom validation logic
+        if not field_of_study.strip():
+            raise ValueError("Field of study cannot be empty.")
+        return field_of_study
 
 class Mission(db.Model, SerializerMixin):
     __tablename__ = 'missions'
@@ -67,15 +80,21 @@ class Mission(db.Model, SerializerMixin):
     planet = db.relationship('Planet', back_populates='missions')
 
     # Add serialization rules
-    serialize_rules = ('-scientist.missions', '-planet.missions', '-planet.id',)
+    serialize_rules = ('-scientist.missions', '-planet.missions', '-planet.id', '-scientist.planets',)
 
-    # Add validation
+    # Add validation for name, scientist_id, and planet_id
     @validates('name')
     def validate_name(self, key, name):
         # custom validation logic
-        if len(name.strip()) == 0:
+        if not name.strip():
             raise ValueError("Name cannot be empty.")
         return name
 
+    @validates('scientist_id', 'planet_id')
+    def validate_foreign_key(self, key, value):
+        # custom validation logic
+        if not value:
+            raise ValueError(f"{key.replace('_id', '').title()} cannot be empty.")
+        return value
 
 # add any models you may need.
